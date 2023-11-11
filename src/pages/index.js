d74846073031b0fc05b6f7def7fee0f4b8438db1
Abhiramivv <abhiramivv77@gmail.com>
2023-11-11 10:06:00 +0530

    chat page design

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,15 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   consdt [inputValue,setInputValue] = useState('');
+  const [chatLog,setChatLog]= useState([]);
+  const [isLoading,setIsloading] = useState(false)
+
   return (
    <>
    <h1>ChatGPT</h1>
    <form>
-    <input type='text' placeholder='Type your message....'/>
+    <input type='text' placeholder='Type your message....' value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+    <button type="submit">Send</button>
    </form>
    </>
   )
