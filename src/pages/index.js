c1e16fd65cbb9dd9f52cb1f54ad68ba2f641612b
Abhiramivv <abhiramivv77@gmail.com>
2023-11-23 12:41:33 +0530

    error handling in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,15 +14,31 @@ export default function Home() {
     event.preventDefault();
 
     setChatLog((prevChatLog) => [...prevChatLog,{type:'user',message:inputValue}])
+    sendMessage(inputValue);
     setInputValue('');
   }
 
   const sendMessage = (message) => {
-    const url ='https://api.openai.com/v1/completions';
+    const url ='https://api.openai.com/v1/chat/completions';
     const headers = {
       'Content-type' : 'application/json',
       'Authorization' :`Bearer ${process.env.NEXT_PUBLIC_API_KEY}`
-    }
+    };
+    const data ={
+      model : "gpt-4",
+      messages : [{"role":"user","content":message}]
+    };
+    setIsloading(true);
+
+    axios.post(url,data,{headers:headers}).then((response)=>{
+      console.log(response);
+      setChatLog((prevChatLog)=>[...prevChatLog ,{type:'bot',message:response.data.choices[0].message.content}])
+      setIsloading(false);
+    }).catch((error)=>{
+      setIsloading(false);
+      console.log(error);
+    })
+
   }
   return (
    <>
