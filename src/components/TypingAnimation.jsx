a6ad98f5d8e77123a8d5121756a1ebe6341e5351
Abhiramivv <abhiramivv77@gmail.com>
2023-11-23 16:09:46 +0530

    typing animation

---
+++ src/components/TypingAnimation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function TypingAnimation() {
+  return (
+    <div className='flex items-center space-x-2'>
+        <div className='w-4 h-4 rounded-full bg-gradient-t-r from-gray-400 to-gray-600 animate-pulse'></div>
+        <div className='w-4 h-4 rounded-full bg-gradient-t-r from-gray-400 to-gray-600 animate-pulse delay-75'></div>
+        <div className='w-4 h-4 rounded-full bg-gradient-t-r from-gray-400 to-gray-600 animate-pulse delay-150' ></div>
+      
+    </div>
+  )
+}
+
+export default TypingAnimation
